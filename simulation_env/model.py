739325d1a2f1b764ec3f4116293e906d38e519d5
Nadun Yashmika <yashnadun@gmail.com>
2024-03-11 01:25:06 +0530

    client and model

--- simulation_env/model.py
+++ simulation_env/model.py
@@ -1,64 +1,184 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import seaborn as sns
+import matplotlib.pyplot as plt
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from sklearn.metrics import classification_report, confusion_matrix
 
-# Note the model and functions here defined do not have any FL-specific components.
+class LSTM(nn.Module):
+    def __init__(self, text_field, dimension=128):
+        super(LSTM, self).__init__()
 
+        self.embedding = nn.Embedding(len(text_field.vocab), 300)
+        self.dimension = dimension
+        self.lstm = nn.LSTM(input_size=300,
+                            hidden_size=dimension,
+                            num_layers=1,
+                            batch_first=True,
+                            bidirectional=True)
+        self.drop = nn.Dropout(p=0.5)
 
-class Net(nn.Module):
-    """A simple CNN suitable for simple vision tasks."""
+        self.fc = nn.Linear(2*dimension, 1)
 
-    def __init__(self, num_classes: int) -> None:
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, num_classes)
+    def forward(self, text, text_len):
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 4 * 4)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        text_emb = self.embedding(text)
 
+        packed_input = pack_padded_sequence(text_emb, text_len, batch_first=True, enforce_sorted=False)
+        packed_output, _ = self.lstm(packed_input)
+        output, _ = pad_packed_sequence(packed_output, batch_first=True)
 
-def train(net, trainloader, optimizer, epochs, device: str):
-    """Train the network on the training set.
+        out_forward = output[range(len(output)), text_len - 1, :self.dimension]
+        out_reverse = output[:, 0, self.dimension:]
+        out_reduced = torch.cat((out_forward, out_reverse), 1)
+        text_fea = self.drop(out_reduced)
 
-    This is a fairly simple training loop for PyTorch.
-    """
-    criterion = torch.nn.CrossEntropyLoss()
-    net.train()
-    net.to(device)
-    for _ in range(epochs):
-        for images, labels in trainloader:
-            images, labels = images.to(device), labels.to(device)
+        text_fea = self.fc(text_fea)
+        text_fea = torch.squeeze(text_fea, 1)
+        text_out = torch.sigmoid(text_fea)
+
+        return text_out
+
+def save_checkpoint(save_path, model, optimizer, valid_loss):
+
+    if save_path == None:
+        return
+    
+    state_dict = {'model_state_dict': model.state_dict(),
+                  'optimizer_state_dict': optimizer.state_dict(),
+                  'valid_loss': valid_loss}
+    
+    torch.save(state_dict, save_path)
+    print(f'Model saved to ==> {save_path}')
+
+def load_checkpoint(load_path, model, optimizer, device):
+
+    if load_path==None:
+        return
+    
+    state_dict = torch.load(load_path, map_location=device)
+    print(f'Model loaded from <== {load_path}')
+    
+    model.load_state_dict(state_dict['model_state_dict'])
+    optimizer.load_state_dict(state_dict['optimizer_state_dict'])
+    
+    return state_dict['valid_loss']
+
+def save_metrics(save_path, train_loss_list, valid_loss_list, global_steps_list):
+
+    if save_path == None:
+        return
+    
+    state_dict = {'train_loss_list': train_loss_list,
+                  'valid_loss_list': valid_loss_list,
+                  'global_steps_list': global_steps_list}
+    
+    torch.save(state_dict, save_path)
+    print(f'Model saved to ==> {save_path}')
+
+def load_metrics(load_path, device):
+
+    if load_path==None:
+        return
+    
+    state_dict = torch.load(load_path, map_location=device)
+    print(f'Model loaded from <== {load_path}')
+    
+    return state_dict['train_loss_list'], state_dict['valid_loss_list'], state_dict['global_steps_list']
+
+def train(model,
+          optimizer,
+          train_loader,
+          valid_loader,
+          num_epochs,
+          eval_every,
+          file_path,
+          device,
+          criterion = nn.BCELoss(),
+          best_valid_loss = float("Inf")):
+    
+    # initialize running values
+    running_loss = 0.0
+    valid_running_loss = 0.0
+    global_step = 0
+    train_loss_list = []
+    valid_loss_list = []
+    global_steps_list = []
+
+    # training loop
+    model.train()
+    for epoch in range(num_epochs):
+        for (labels, (title, title_len), (text, text_len), (titletext, titletext_len)), _ in train_loader:           
+            labels = labels.to(device)
+            titletext = titletext.to(device)
+            titletext_len = titletext_len.to(device)
+            output = model(titletext, titletext_len)
+
+            loss = criterion(output, labels)
             optimizer.zero_grad()
-            loss = criterion(net(images), labels)
             loss.backward()
             optimizer.step()
 
+            # update running values
+            running_loss += loss.item()
+            global_step += 1
 
-def test(net, testloader, device: str):
-    """Validate the network on the entire test set.
+            # evaluation step
+            if global_step % eval_every == 0:
+                model.eval()
+                with torch.no_grad():                    
+                  # validation loop
+                  for (labels, (title, title_len), (text, text_len), (titletext, titletext_len)), _ in valid_loader:
+                      labels = labels.to(device)
+                      titletext = titletext.to(device)
+                      titletext_len = titletext_len.to(device)
+                      output = model(titletext, titletext_len)
 
-    and report loss and accuracy.
-    """
-    criterion = torch.nn.CrossEntropyLoss()
-    correct, loss = 0, 0.0
-    net.eval()
-    net.to(device)
+                      loss = criterion(output, labels)
+                      valid_running_loss += loss.item()
+
+                # evaluation
+                average_train_loss = running_loss / eval_every
+                average_valid_loss = valid_running_loss / len(valid_loader)
+                train_loss_list.append(average_train_loss)
+                valid_loss_list.append(average_valid_loss)
+                global_steps_list.append(global_step)
+
+                # resetting running values
+                running_loss = 0.0                
+                valid_running_loss = 0.0
+                model.train()
+
+                print('Epoch [{}/{}], Step [{}/{}], Train Loss: {:.4f}, Valid Loss: {:.4f}'
+                      .format(epoch+1, num_epochs, global_step, num_epochs*len(train_loader),
+                              average_train_loss, average_valid_loss))
+                
+                # checkpoint
+                if best_valid_loss > average_valid_loss:
+                    best_valid_loss = average_valid_loss
+                    save_checkpoint(file_path + '/model.pt', model, optimizer, best_valid_loss)
+                    save_metrics(file_path + '/metrics.pt', train_loss_list, valid_loss_list, global_steps_list)
+    
+    save_metrics(file_path + '/metrics.pt', train_loss_list, valid_loss_list, global_steps_list)
+    print('Finished Training!')
+
+def test(model, test_loader, device, version='title', threshold=0.5):
+    y_pred = []
+    y_true = []
+
+    model.eval()
     with torch.no_grad():
-        for data in testloader:
-            images, labels = data[0].to(device), data[1].to(device)
-            outputs = net(images)
-            loss += criterion(outputs, labels).item()
-            _, predicted = torch.max(outputs.data, 1)
-            correct += (predicted == labels).sum().item()
-    accuracy = correct / len(testloader.dataset)
-    return loss, accuracy
+        for (labels, (title, title_len), (text, text_len), (titletext, titletext_len)), _ in test_loader:           
+            labels = labels.to(device)
+            titletext = titletext.to(device)
+            titletext_len = titletext_len.to(device)
+            output = model(titletext, titletext_len)
+
+            output = (output > threshold).int()
+            y_pred.extend(output.tolist())
+            y_true.extend(labels.tolist())
+    
+    report = classification_report(y_true, y_pred, labels=[1,0], digits=4)
+    print('Classification Report:')
+    print(report)
+    return report
---
+++ simulation_env/strategy.py
@@ -0,0 +1,155 @@
+from typing import Dict, List, Tuple, Union, Optional
+import flwr as fl
+import json
+
+from flwr.server.client_proxy import ClientProxy
+
+from init_devices import get_device_parameters
+from device_selection import Device, compute_metrics, select_devices_UAC
+
+from flwr.common import FitRes, Parameters, FitIns, Scalar, parameters_to_ndarrays
+from flwr.server.client_manager import ClientManager
+from logging import DEBUG, INFO
+from flwr.common.logger import log
+from types import Dict
+
+devices_list = []
+device_selection = "overall"
+sample_device_per = 0.5
+user_model_path = './checkpoints'
+seq_len = 10
+input_dim = 100
+WAIT_TIMEOUT = 600
+
+class PersonalizationStrategy(fl.server.strategy.FedAvg):
+    def __init__(
+            self,
+            fraction_fit: float = 0.1,
+            fraction_eval: float = 0.1,
+            min_fit_clients: int = 1,
+            min_eval_clients: int = 1,
+            min_available_clients: int = 1,
+            eval_fn = None,
+            on_fit_config_fn = None,
+            on_evaluate_config_fn = None,
+            initial_parameters = None,
+            num_clients: int = 105,
+    ) -> None:
+        super().__init__(
+            fraction_fit=fraction_fit,
+            fraction_eval=fraction_eval,
+            min_fit_clients=min_fit_clients,
+            min_eval_clients=min_eval_clients,
+            min_available_clients=min_available_clients,
+            eval_fn=eval_fn,
+            on_fit_config_fn=on_fit_config_fn,
+            on_evaluate_config_fn=on_evaluate_config_fn,
+            initial_parameters=initial_parameters
+        )
+        try:
+            num_device_per_user = len(devices_list)
+            device_configs, device_speeds = get_device_parameters(num_clients, num_device_per_user)
+            self.devices = [Device(device_configs[i], device_speeds[i // num_device_per_user - 1])
+                                for i in range(num_clients)]
+            self.losses = []
+            self.round_times = []
+            self.sampled_indices = []
+
+            self.init_loss, self.init_drain = compute_metrics(self.devices)
+            with open("{}/init.json".format('results'), 'w') as json_file:
+                round_data = {
+                    'rnd': 'init',
+                    'total_loss': self.init_loss,
+                    'dead_devices': self.init_drain,
+                    'device_snapshot': [
+                        {
+                            'device_id': i,
+                            'drain': d.drain,
+                            't_local': d.t_local
+                        } for i, d in enumerate(self.devices)
+                    ]
+                }
+                json.dump(round_data, json_file)
+        except Exception as e:
+            print('Something happened in Strategy init')
+            print(e)
+
+        
+    def configure_fit(self, rnd: int, parameters: Parameters, client_manager: ClientManager):
+        if device_selection == 'none':
+            return super().configure_fit(rnd, parameters, client_manager)
+        else:
+            try:
+                sample_size, min_num_clients = self.num_fit_clients(client_manager.num_available())
+                success = client_manager.wait_for(num_clients=min_num_clients, timeout=WAIT_TIMEOUT)
+                if not success:
+                # Do not continue if not enough clients are available
+                    log(INFO,
+                        "Not enough clients available after timeout %s",
+                        WAIT_TIMEOUT
+                        )
+                    return []
+                
+                # Sample clients
+                msg = "Round %s, sample %s clients (based on device selection criteria)"
+                log(DEBUG, msg, str(rnd), str(sample_size))
+                all_clients = client_manager.all()
+                cid_idx: Dict[int, str] = {}
+                for idx, (cid, _) in enumerate(all_clients.items()):
+                    cid_idx[idx] = cid
+                    # print("All clients cid: {}, idx: {}".format(cid, idx))
+
+                global CURR_ROUND
+                CURR_ROUND = rnd
+                if rnd == 1:
+                    # sampled_indices = select_devices(rnd, devices=self.devices, strategy='random')
+                    sampled_indices = [*range(len(all_clients))]
+                else:
+                    num_device_per_user = len(devices_list)
+                    num_sample_devices = int(len(self.devices) * sample_device_per)
+                    print("NUM DEVICE PER USER SAMPLED : ", num_device_per_user)
+                    sampled_indices = select_devices_UAC(num_devices=num_sample_devices, devices=self.devices, strategy=device_selection,
+                                                        num_device_per_user=num_device_per_user)
+                
+                print('sampled indices: ', sampled_indices)
+                self.sampled_indices = sampled_indices
+
+                round_time = 0
+                for idx in sampled_indices:
+                    if self.devices[idx].t_local > round_time:
+                        round_time = self.devices[idx].t_local
+
+                    self.devices[idx].update_local_state(rnd)
+                    self.round_times.append(round_time)
+
+                config = {}
+                if self.on_fit_config_fn is not None:
+                    config = self.on_fit_config_fn(rnd)
+
+                FitIns(parameters, config)
+            except Exception as e:
+                print("Something wrong with CONFIGURE FIT")
+                print(e)
+            return super().configure_fit(rnd, parameters, client_manager)
+    
+    def aggregate_fit(self,
+                    server_round: int,
+                    results: List[Tuple[ClientProxy, FitRes]],
+                    failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]]
+                    ) -> Tuple[Optional[Parameters], Dict[str, Scalar]]:
+        try:
+            print("Aggeregate fit is called")
+            if not results:
+                return None, {}
+            
+            global_weights_results = {}
+
+            if device_selection != 'none':
+                for client, r in results:
+                    if int(client.cid) in self.sampled_indices:
+                        self.devices[int(client.cid)].update_num_samples(r.num_examples)
+                        self.devices[int(client.cid)].update_loss(r.metrics['loss'])
+                        global_weights_results.append((parameters_to_ndarrays(r.parameters), r.num_examples))
+
+                    if user_model_path != 'no_personal' and user_model_path != 'local_finetuning':
+                        input_shape = sequaance_len
